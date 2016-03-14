bf95234e37a4078fac1853ba2dba6eba32a0e782
lionellbriones <llenoil@gmail.com>
2016-03-15 00:25:55 +0800

    removed contact test files

--- src/app/contact.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from "angular2/core";
-import {CONTACTS} from "./mock-contact";
-
-@Injectable()
-export class ContactService {
-  getContacts(){
-    return Promise.resolve(CONTACTS);
-  }
-}
--- src/app/mock-contact.ts
+++
@@ -1,8 +0,0 @@
-import {Contact} from './contact';
-
-export const CONTACTS: Contact[] = [
-  {firstName: "Lionell", lastName: "Briones", phone: "09355452556", email: "llen@gmail.com"},
-  {firstName: "Babylo", lastName: "Amazona", phone: "09355452556", email: "ba@gmail.com"},
-  {firstName: "Leo", lastName: "Briones", phone: "09355452556", email: "leo@gmail.com"},
-  {firstName: "Andre", lastName: "Bayana", phone: "09355452556", email: "dre@gmail.com"},
-];
